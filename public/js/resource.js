88281e644c0bd5fc1de5b4d5f7f87c25f9b38ddd
Celentano <CD115986@ardp.local>
2018-11-23 18:02:22 -0300

    I've just created the resource class template

---
+++ public/js/resource.js
@@ -0,0 +1,26 @@
+/**
+ * A class that wraps up our resources logic. It creates and animates a sprite in
+ * response to Player Actions. Call its update method from the scene's update and call its destroy
+ * method when you're done with the resource.
+ */
+export default class Resource {
+  constructor(scene, type){
+    this.scene = scene;
+    this.amount = this.randomAmount();
+    this.state = "available";
+    //this.sprite =
+  }
+  update(){
+    //TODO Create update for resource.
+  }
+  consumed(){
+      //TODO make the resource consumed and change the sprite and clock to renovation.
+  }
+
+  destroy(){
+    //TODO Destroy the resources.
+  }
+  randomAmount(){
+    //TODO return random number between accepted values
+  }
+}
